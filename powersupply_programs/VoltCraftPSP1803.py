62befefff19e79ee55c29f320cb3891f6b3d78cb
ak-eder <123975243+ak-eder@users.noreply.github.com>
2023-01-30 17:30:33 +0100

    Add documentation to the VoltCraft Class

--- powersupply_programs/VoltCraftPSP1803.py
+++ powersupply_programs/VoltCraftPSP1803.py
@@ -1,7 +1,20 @@
 import serial
 
 
-class VoltCraft:
+class VoltCraftPSP1803:
+    '''
+    This class is used to handele the commuication with a Voltcraft PSP1803 via Serial.
+
+    ...
+
+    Attributes
+    ----------
+    port 
+        Port the serial device is connected to
+
+    Methods
+    -------
+    '''
 
     def __init__(self, port:str='COM1') -> None:
         
@@ -15,6 +28,14 @@ class VoltCraft:
         )
     
     def set_voltage(self,V:float) -> None:
+        """Sets the outputvoltage in Volt.
+
+        Parameters
+        ----------
+        V
+            Voltage to be set in Volt
+        """
+        
         send = b''
         send += b'\xaa'
         V = round(V*50)
@@ -22,6 +43,13 @@ class VoltCraft:
         self.ser.write(send)
 
     def read_voltage(self) -> float:
+        """Reads the set Volatge.
+
+        Returns
+        -------
+        float
+            Volatge in Volt
+        """
         send = b''
         send += b'\xae'
         send += b'\x00'
@@ -31,6 +59,9 @@ class VoltCraft:
         return (int.from_bytes(self.ser.read(3)[1:3], byteorder='big')/50)
 
     def enable_source(self) -> None:
+        """Activates the output. 
+
+        """
         send = b''
         send += b'\xab'
         send += b'\x01'
@@ -38,6 +69,9 @@ class VoltCraft:
         self.ser.write(send)
 
     def disable_source(self) -> None:
+        """Deactivate the output. 
+
+        """
         send = b''
         send += b'\xab'
         send += b'\x00'
